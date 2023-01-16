c997e6b4fa23a1792b7d91abc240ef71d2419280
Contentcreatorz <c.oduk@outlook.com>
2023-01-16 14:44:14 +0000

    Styled and added article voting buttons

--- src/components/Article/comments/comments.jsx
+++ src/components/Article/comments/comments.jsx
@@ -7,34 +7,35 @@ export const CommentsList = ({ comments: { comments } }) => (
     < section className="comments-section" >
         <h1 className="section-title">Comments</h1>
 
-        {
-            comments.map(({ comment_id, created_at, author, votes, body }) => (
-                <article className="comment-wrap" id={`comment-${comment_id}`}>
-                    <header className="comment-content">
-                        <div className="comment-author-wrap">
-                            <h2 className="comment-author">{author}</h2>
-                            <time className="comment-time" dateTime={created_at}>
-                                {moment(created_at).format('D/M/YYYY')}
-                            </time>
-                        </div>
-                        <div className="comment-body">
-                            <p>{body}</p>
-                        </div>
-                    </header>
-                    <footer className="comment-actions">
-                        <div className="votes-container">
-                            <span className="votes">Votes: {votes}</span>
-                            <br />
-                            <span className="votes-up">Vote ✔</span>
-                            <br />
-                            <span className="votes-down">Vote ✖</span>
-                        </div>
-                        <div className="comment-delete">
-                            Delete 🗑
-                        </div>
-                    </footer>
-                </article>
-            ))
-        }
+        {comments.map(({ comment_id, created_at, author, votes, body }) => (
+            <article className="comment-wrap" key={`comment-${comment_id}`}>
+                <header className="comment-content">
+                    <div className="comment-meta">
+                        <h2 className="comment-author">
+                            {author}
+                        </h2>
+                        <time className="comment-time" dateTime={created_at}>
+                            {moment(created_at).format('D/M/YYYY')}
+                        </time>
+                    </div>
+                    <div className="comment-body">
+                        <p>
+                            {body}
+                        </p>
+                    </div>
+                </header>
+                <footer className="comment-actions">
+                    <div className="votes-container">
+                        <span className="votes">
+                            Votes: {votes}
+                        </span>
+                        <br />
+                    </div>
+                    <div className="comment-delete">
+                        Delete 🗑
+                    </div>
+                </footer>
+            </article>
+        ))}
     </section >
 )    
--- src/components/Article/singleArticle/article.jsx
+++ src/components/Article/singleArticle/article.jsx
@@ -2,7 +2,7 @@ import moment from "moment";
 import './style.css'
 
 export const Article = ({
-    article: { title, topic, authorName, body, comment_count, created_at }
+    article: { title, topic, authorName, body, votes, created_at }
 }) => (
     <article className="Article">
         <div className="article-topic" itemProp="articleSection">
@@ -10,16 +10,17 @@ export const Article = ({
         </div>
         <header>
             <h1 className="article-title" itemProp="headline">{title}</h1>
-            <div className="article-meta">
-                <div className="article-author" itemProp="author">
-                    {`Published by ${authorName} on ${moment(created_at).format('MMMM D, YYYY')}`}
-                </div>
+            <div className="article-author" itemProp="author">
+                {`Published by ${authorName} on ${moment(created_at).format('MMMM D, YYYY')}`}
             </div>
         </header>
         <div className="article-body" itemProp="articleBody">
             {body}
         </div>
+        <span className="votes-total">{`Total Votes 🗳: ${votes}`} </span>
         <footer className="article-meta">
+            <span className="votes-up">Vote ✔</span>
+            <span className="votes-down">Vote ✖</span>
             {/* <div className="article-comment_count" itemProp="commentCount">
                 {formatCommentCount(comment_count)}
             </div> */}
